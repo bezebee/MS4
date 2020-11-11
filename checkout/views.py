69bcb3e1f82e558572f1973a4adbbe4f8f177e55
Kasia Bee <kas.bogucka@gmail.com>
2020-11-11 07:43:54 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key' :'pk_test_51HgGdmEvXyoFf9L0daCV8JJ4gt5pzWjDWMADjQhyPTJyzpNtCqeNyyVoiri4FFe04Pmr8eGUThLzgTbvo3tfkCgF00gDahkHI5',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
