42e0f89e05dff17c613964a8d4917927d73854d8
Kasia Bee <kas.bogucka@gmail.com>
2020-11-15 17:47:21 +0000

    secret key removed and debug set

--- MS4/settings.py
+++ MS4/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'k57ersxb$w)^h7!wyz)k@ipk^h@2+mx^$9yc_v1r0cw9+ukxg1'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['ms4nicknacks.herokuapp.com', 'localhost']
 
