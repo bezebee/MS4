3b049b1a153fe348eeb6884356160570df946f86
Kasia Bee <kas.bogucka@gmail.com>
2020-11-17 21:36:28 +0000

    Added User Designs views and templates

--- designs/views.py
+++ designs/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Design
 
 # Create your views here.
+def all_designs(request):
+    """A view to show all user's designs and searching activities"""
+
+    designs = Design.objects.all()
+
+    context = {
+        'designs': designs,
+    }
+
+    return render(request,'designs/designs.html', context)
