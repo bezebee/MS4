92d442846e0f62afb92f590a8cc45714f383e38d
Kasia Bee <kas.bogucka@gmail.com>
2020-11-21 16:56:29 +0000

    Added Design details functionality

--- designs/urls.py
+++ designs/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_designs, name="designs")
+    path('', views.all_designs, name="designs"),
+    path('<design_id>', views.design_detail, name="design_detail"),
 ]
--- designs/views.py
+++ designs/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Design
 
 # Create your views here.
@@ -12,3 +12,15 @@ def all_designs(request):
     }
 
     return render(request,'designs/designs.html', context)
+
+
+def design_detail(request, design_id):
+    """A view to show user design, including ordering the custom design"""
+
+    design = get_object_or_404(Design, pk=design_id)
+
+    context = {
+        'design': design,
+    }
+
+    return render(request,'designs/design_detail.html', context)
