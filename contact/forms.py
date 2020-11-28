aadd71fa012bf28131a4c64ed9173370c439b4b3
Kasia Bee <kas.bogucka@gmail.com>
2020-11-28 20:19:21 +0000

    Used attributes and css to style a default form from Django

--- contact/forms.py
+++ contact/forms.py
@@ -4,6 +4,11 @@ from .models import Contact
 
 
 class ContactForm(forms.ModelForm):
+    first_name=forms.CharField(label="", widget=forms.TextInput(attrs={'placeholder':'First name'}))
+    last_name=forms.CharField(label="", widget=forms.TextInput(attrs={'placeholder':'Last name'}))
+    email=forms.CharField(label="", widget=forms.TextInput(attrs={'placeholder':'Email address'}))
+    message=forms.CharField(label="", widget=forms.Textarea(attrs={'placeholder':'Message'}))
     class Meta:
         model = Contact
         fields = ('first_name', 'last_name', 'email', 'message')
+     
