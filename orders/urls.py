978b7104f2421c4b6b80a3fd96aca178e825cd8a
Kasia Bee <kas.bogucka@gmail.com>
2020-12-12 15:29:36 +0000

    Added order design functionality

--- orders/urls.py
+++ orders/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.view_orders, name='view_orders'),
-
+    path('add/<order_id>/', views.order, name='order'),
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -6,3 +6,20 @@ def view_orders(request):
     """ A view that renders the orders contents page """
 
     return render(request, 'orders/orders.html')
+
+
+def order(request, order_id):
+    """Add quantity of the specified design to the order"""
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    orders = request.session.get('orders', {})
+
+    if order_id in list(orders.keys()):
+        orders[order_id] += quantity
+    else:
+        orders[order_id] = quantity
+
+    request.session['orders'] = orders
+    print(request.session['orders'])
+    return redirect(redirect_url)
