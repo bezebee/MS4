c7619db4038cd1717ffbe945a91996b37ff22a20
Kasia Bee <kas.bogucka@gmail.com>
2020-12-12 17:18:32 +0000

    Added slash to the design path, trying to investigate 500 error on Heroku

--- designs/urls.py
+++ designs/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.all_designs, name="designs"),
     path('<int:design_id>/', views.design_detail, name="design_detail"),
-    path('add', views.add_design, name="add_design"),
+    path('add/', views.add_design, name="add_design"),
     path('edit/<int:design_id>/', views.edit_design, name="edit_design"),
     path('delete/<int:design_id>/', views.delete_design, name="delete_design"),
 ]
