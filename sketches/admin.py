1d149c2cb9a8b00d9dcc3b5772664a04a3e9d6f3
Kasia Bee <kas.bogucka@gmail.com>
2020-12-14 20:32:54 +0000

    Customised Admin

--- sketches/admin.py
+++ sketches/admin.py
@@ -2,5 +2,25 @@ from django.contrib import admin
 from .models import Sketch, Sheet
 
 # Register your models here.
-admin.site.register(Sketch)
-admin.site.register(Sheet)
+
+class SketchAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'sheet',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+class SheetAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Sketch, SketchAdmin)
+admin.site.register(Sheet, SheetAdmin)
