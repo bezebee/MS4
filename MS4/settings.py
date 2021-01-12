fa37ac2c5d0581779ab6095cd6bbfb8d46590e3a
Kasia Bee <kas.bogucka@gmail.com>
2021-01-12 14:57:54 +0000

    Removed default from email from settings.py

--- MS4/settings.py
+++ MS4/settings.py
@@ -217,7 +217,7 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'kas.bogucka@gmail.com'
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
