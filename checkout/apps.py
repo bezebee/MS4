cc09d691c978cc8e95f63bb405fc944b67b67fe0
bezebee <kasia79ie@gmail.com>
2026-05-05 21:53:44 +0100

    rename checkout app label to avoid conflict

--- checkout/apps.py
+++ checkout/apps.py
@@ -3,7 +3,7 @@ from django.apps import AppConfig
 
 class CheckoutConfig(AppConfig):
     name = 'checkout'
-    label = 'checkout'
+    label = 'store_checkout'
     default_auto_field = 'django.db.models.BigAutoField'
 
     def ready(self):
