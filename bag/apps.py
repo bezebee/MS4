dfa4e9f77d57aa2e4a30e1b20911c245d6e55533
bezebee <kasia79ie@gmail.com>
2026-05-05 22:07:14 +0100

    fix bag/apps.py — was incorrectly using CheckoutConfig

--- bag/apps.py
+++ bag/apps.py
@@ -1,9 +1,5 @@
 from django.apps import AppConfig
 
 
-class CheckoutConfig(AppConfig):
-    name = 'checkout'
-
-    def ready(self):
-        import checkout.signals
-
+class BagConfig(AppConfig):
+    name = 'bag'
