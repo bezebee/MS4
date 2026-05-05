2354f81273d7ceebbe0d7cb952aaa6536a4f2821
bezebee <kasia79ie@gmail.com>
2026-05-05 21:50:40 +0100

    try django-allauth 0.44.0 to fix checkout label conflict

--- checkout/apps.py
+++ checkout/apps.py
@@ -3,6 +3,8 @@ from django.apps import AppConfig
 
 class CheckoutConfig(AppConfig):
     name = 'checkout'
+    label = 'checkout'
+    default_auto_field = 'django.db.models.BigAutoField'
 
     def ready(self):
         import checkout.signals
